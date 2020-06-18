6bc05498cab19d6ba7da545ca81a71e05acf1855
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-06-19 01:42:23 +0500

    update app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+//CHAPTER 21 - 25
+
+
+//Task 01
+// var firstName = prompt("Enter your first Name ")
+// var lastName = prompt("Enter your Last Name ")
+// alert("Hello "+ firstName +" "+ lastName)
+
+//Task 02
+// var mob = prompt("Your favourite smartPhone? ")
+// document.write("My Favourite smart Phone is: "+ mob +"<br>"+"Length of string: "+ mob.length)
+
+//Task 03
+// var str = "Pakistani"
+// document.write("String: "+ str +"<br> Index of 'n' : "+ str.indexOf('n'))
+
+//Task 04
+// var str = "Hello World"
+// document.write("String: "+ str +"<br> Last index of 'l' : "+ str.lastIndexOf('l'))
+
+//Task 05
+// var str = "Pakistani"
+// document.write("String: "+ str +"<br> Character at index 3 : "+ str.charAt(3))
+
+//Task 06
+// var firstName = prompt("Enter your first Name ")
+// var lastName = prompt("Enter your Last Name ")
+// alert("Hello ".concat(firstName, lastName))
+
+//Task 07
+// var str1 = "Hyderabad"
+// var str2 = str1.slice(str1.indexOf('a')) //abad
+// var str3 = str2.replace(str2, 'Islam')//Islam
+// var str4 = str3.concat(str2)//Islamabad
+// document.write("City: "+ str1 +"<br> After replacement: "+ str4)
+
+//Task 08
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+// var str = message.replace(/and/g, '&') // "/and/g" shows the globa;l replacement with following text
+// document.write(str)
+
+//Task 09
+// var str = "472"
+// var num = Number(str)
+// document.write("Value: "+ str +"<br>"+ "Type: "+typeof(str) +"<br>"+ "Value: "+ num +"<br>"+"Type: "+typeof(num))
+
+//Task 10
+// var str = prompt("Type dummy text in small letters & convert it into Capital ")
+// var  str2 = str.toUpperCase()
+// document.write("User input: "+ str +"<br>"+ "Upper Case: "+ str2)
+
+//Task 11
+// var str = prompt("Type string & convert it into Title Case ")
+// var str2 = (str.slice(0, 1)).toUpperCase()
+// var str3 = str2.concat(str.slice(1))
+// document.write("User input: "+ str +"<br>"+ "Title Case: "+ str3)
+
+//Task 12
+// var num = 35.36 //first we convert this number in to string using toString otherwise they read as a number & not remove dot
+// document.write("Number: "+ num +"<br>"+ "Result: "+ num.toString().replace('.',""))
+
+//Task 13
+
+
+
+
+
+//Task 12
+//Task 12
+//Task 12
+//Task 12
+//Task 12
+//Task 12
+//Task 12
+//Task 12
+
